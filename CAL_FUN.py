5ede1f3f8e4d47af47b59eda997ff1cae3eeb667
VINU2021 <86190134+VINU2021@users.noreply.github.com>
2021-12-28 23:08:51 +0530

    VINUSHREE.S ass1.2

--- CAL_FUN.py
+++ CAL_FUN.py
@@ -1,3 +1,5 @@
+--CALCULATOR--
+
 input("select operation from the following : 1-add , 2-sub , 3-mult ,4- div :  " )
 if x=='1':
  a=int(input("enter a number : "))
@@ -21,3 +23,19 @@ elif x=='4':
  print("the multiplication of given 2 number is ",a/b)
 else:
  print("the given input is invalid")
+ 
+ --FUNCTION WITH 3 PARAMETER---
+ def areaoftriangle(x,y,z):
+ p=x+y+z
+ print("the perimeter if the triangle is : ",p)
+ s=p/2
+ print("the semi-perimeter of the triangle is : ",s)
+ a=(s*(s-x)*(s-y)*(s-z))*0.5
+ print("the area of the triangle is :",a)
+
+print("area of a triangle ")
+h=float(input("enter the 1st side of triangle : "))
+b=float(input("enter the 2nd side of triangle : "))
+w=float(input("enter the 3rd side of triangle : "))
+areaoftriangle(h,b,w)
+ 
