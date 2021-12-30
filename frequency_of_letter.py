b16f0c2578eb1130b324f8b504baf6f1011ef61a
VINU2021 <86190134+VINU2021@users.noreply.github.com>
2021-12-30 22:00:57 +0530

    vinushree.S edited

--- frequency_of_letter.py
+++ frequency_of_letter.py
@@ -1,9 +1,11 @@
 import operator
-string = input("enter a string :")
-d = dict()
-for letter in string:
-    if letter not in d:
-        d[letter] = string.count(letter)
+s = input("enter a string :")
+def most_frequent_letter(string):
+  d = dict()
+  for letter in s:
+     if letter not in d:
+        d[letter] = s.count(letter)
 # Sort dictionary by values
-ordered_answer = sorted(d.items(), key=operator.itemgetter(1), reverse=True)
-print (ordered_answer)
+  ordered_answer = sorted(d.items(), key=operator.itemgetter(1), reverse=True)
+  print (ordered_answer)
+print(most_frequent_letter(s))
