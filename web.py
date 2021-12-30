6df27fcc7db22640587725bb86ee73ca761faf7b
VINU2021 <86190134+VINU2021@users.noreply.github.com>
2021-12-30 13:05:50 +0530

    vinushree.S

---
+++ web.py
@@ -0,0 +1,6 @@
+import requests
+url = 'https://analytics.usa.gov/data/live/realtime.json'
+j = requests.get(url).json()
+print("Number of people visiting a U.S. government website-")
+print("Active Users Right Now:")
+print(j['data'][0]['active_visitors'])
